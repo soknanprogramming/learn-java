56aa1a1a8d4cd16159b5b57241af5f797fd8d369
soknanprogramming <poch.soknan@gmail.com>
2025-09-14 06:29:09 +0700

    f

--- crud/src/main/java/com/example/texting/models/Product.java
+++ crud/src/main/java/com/example/texting/models/Product.java
@@ -1,5 +1,27 @@
 package com.example.texting.models;
 
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
+
+    @Id
+    private int id;
+    private String name;
+    private String desc;
+    private int price;
+    private String category;
+    private Date releaseDate;
+    private boolean available;
+    private int quantity;
 
 }
